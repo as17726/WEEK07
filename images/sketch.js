dbc571e37e5286a3fea07da9097a2bda0af79b7b
Ayushi Shah <as17726@nyu.edu>
2023-10-18 15:01:15 -0400

    changing colors

--- images/sketch.js
+++ images/sketch.js
@@ -7,9 +7,32 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
+  mImg.resize(0, height); 
+  mImg.loadPixels(); 
+  print(mImg.pixels.length); 
+  noLoop(); 
 }
 
 function draw() {
   background(220, 20, 120);
-  image(mImg, 0, 0);
+
+  for(let vi = 0; vi<mImg.pixels.length; vi+=4) {
+    let redVal = mImg.pixels[vi]; 
+    let greenVal = mImg.pixels[vi+1]; 
+    let blueVal = mImg.pixels[vi+2]; 
+    let alphaVal = mImg.pixels[vi+3]; 
+
+    let maxVal = max(redVal, greenVal, blueVal);
+    // if(maxVal == redVal) { 
+    //   mImg.pixels[vi + 0] = 255; 
+    // } else if (maxVal == greenVal) {
+    //   mImg.pixels[vi + 1] = 255; 
+    //}
+     if (maxVal == blueVal) {
+      mImg.pixels[vi + 3] = 0; 
+  }
+}
+  mImg.updatePixels(); 
+  image(mImg,(width - mImg.width)/2, 0);
+
 }
